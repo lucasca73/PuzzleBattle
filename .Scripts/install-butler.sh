b32c1a59b61fa1483f0a7f3a204f389e6dd73a9a
Lucas Costa <lucascostaa73@gmail.com>
2018-12-07 19:09:02 -0200

    Updating butler script

--- .Scripts/install-butler.sh
+++ .Scripts/install-butler.sh
@@ -7,7 +7,7 @@
 curl -L -o butler.zip https://broth.itch.ovh/butler/linux-amd64/LATEST/archive/default
 unzip butler.zip
 # GNU unzip tends to not set the executable bit even though it's set in the .zip
-chmod +x butler
+chmod a+x ./butler
 # just a sanity check run (and also helpful in case you're sharing CI logs)
 ./butler -V
 ./butler login
