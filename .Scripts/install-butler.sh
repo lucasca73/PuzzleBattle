6b81e44ca8ba2f359ca06fba6e0431bcbd7884c7
Lucas Costa <lucascostaa73@gmail.com>
2018-12-07 18:03:22 -0200

    Adding build scripts and Travis ym

---
+++ .Scripts/install-butler.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+# apt-get update && apt-get install unzip
+
+# -L follows redirects
+# -O specifies output name
+curl -L -o butler.zip https://broth.itch.ovh/butler/linux-amd64/LATEST/archive/default
+unzip butler.zip
+# GNU unzip tends to not set the executable bit even though it's set in the .zip
+chmod +x butler
+# just a sanity check run (and also helpful in case you're sharing CI logs)
+./butler -V
