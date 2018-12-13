9c7d9c9f8171620a5cce2fff9c1af5fa1faee329
Lucas Costa <lucascostaa73@gmail.com>
2018-12-13 18:00:00 -0200

    Adding buttons in input and bootstrap

--- joystickServer/joystick-server/src/App.js
+++ joystickServer/joystick-server/src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Button } from 'react-bootstrap';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+var self = undefined
 
 class App extends Component {
 
@@ -7,7 +12,8 @@ class App extends Component {
     super(props);
     this.state = {
       ip_address: '',
-      ws: undefined
+      ws: undefined,
+      move: undefined
     };
   }
 
@@ -32,6 +38,8 @@ class App extends Component {
       // connection opened
       ws.send('Remote activated'); // send a message
       this.setState( {ws: ws} )
+      self = this
+      this.loopInput()
     };
     ws.onclose = (e) => {
       // this.showAlert('Error', 'Não foi possível se conectar com o veículo.')
@@ -44,22 +52,56 @@ class App extends Component {
     this.state.ws.send(`input,${move}`)
   }
 
+  leftDown(e){
+    this.setState({move: 'left'})
+  }
+
+  leftUp(e){
+    this.setState({move: undefined})
+  }
+
+  rightDown(e){
+    this.setState({move: 'right'})
+  }
+
+  rightUp(e){
+    this.setState({move: undefined})
+  }
+  
+  loopInput(){
+
+    if (self.state.move){
+      self.movePiece(self.state.move)
+    }
+
+    if (self.state.ws){
+      setTimeout(self.loopInput, 150)
+    }
+  }
+
+  
   renderButtons(){
     return (
 
       <div className="App">
         <header className="App-header">
 
-          <div className="container">
-              <div className="row">
-                <button 
-                  type="button" 
-                  onClick={ () => this.movePiece('left') }
-                > {'   <   '}  </button>
-                <button 
-                  type="button" 
-                  onClick={ () => this.movePiece('right') }
-                > {'   >   '} </button>
+          {/* <div className="container"> */}
+          <div style={{display: 'flex', justifyContent: 'center'}}>
+              <div className="row" >
+                <Button 
+                  bsStyle="primary"
+                  onMouseDown={(e) => this.leftDown() }
+                  onMouseUp={(e) => this.leftUp() }
+                  style={{display: 'flex', padding:30, margin:30}}
+                > {'   <   '}  </Button>
+                <br/>
+                <Button 
+                  bsStyle="primary"
+                  onMouseDown={(e) => this.rightDown() }
+                  onMouseUp={(e) => this.rightUp() }
+                  style={{display: 'flex', padding:30, margin:30}}
+                > {'   >   '} </Button>
               </div>
           </div>
 
@@ -93,13 +135,19 @@ class App extends Component {
   render() {
 
     var current = this.state.ws ? this.renderButtons() : this.renderConnectButton()
-    console.log(this.state.ws ? 'is connected!' : 'is not connected...')
     return (
+
       <div>
+        <meta 
+          name='viewport'
+          content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' 
+        />
         {current}
       </div>
     );
   }
 }
+
+
 
 export default App;
