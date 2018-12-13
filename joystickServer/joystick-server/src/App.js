4ebc8ae974d5cd4d3e38ebbaec652c7a955f5629
Lucas Costa <lucascostaa73@gmail.com>
2018-12-13 16:00:14 -0200

    Connecting with websocket server and sending movement messages

--- joystickServer/joystick-server/src/App.js
+++ joystickServer/joystick-server/src/App.js
@@ -1,25 +1,102 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      ip_address: '',
+      ws: undefined
+    };
+  }
+
+  componentDidMount(){
+    this.connectToWS('192.168.25.3')
+  }
+
+  handleIpAdrressChange(e){
+    this.setState({ip_address: e.target.value});
+    console.log(this.state.ip_address)
+  }
+
+  handlePressEnter(e){
+    if (e.key === 'Enter') {
+      this.connectToWS(this.state.ip_address)
+    }
+  }
+
+  connectToWS(ip_address){
+    var ws = new WebSocket(`ws://${ip_address}:8000`);
+    ws.onopen = () => {
+      // connection opened
+      ws.send('Remote activated'); // send a message
+      this.setState( {ws: ws} )
+    };
+    ws.onclose = (e) => {
+      // this.showAlert('Error', 'Não foi possível se conectar com o veículo.')
+      console.log(e.code, e.reason);
+      this.setState( {ws: undefined} )
+    };
+  }
+
+  movePiece(move){
+    this.state.ws.send(`input,${move}`)
+  }
+
+  renderButtons(){
     return (
+
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
+          <div className="container">
+              <div className="row">
+                <button 
+                  type="button" 
+                  onClick={ () => this.movePiece('left') }
+                > {'   <   '}  </button>
+                <button 
+                  type="button" 
+                  onClick={ () => this.movePiece('right') }
+                > {'   >   '} </button>
+              </div>
+          </div>
+
         </header>
+      </div>
+    )
+  }
+
+  renderConnectButton(){
+    return (<div className="App">
+    <header className="App-header">
+
+    <form>
+      <input 
+        type="text" 
+        name="ip_adress" 
+        placeholder="192.168.1.2" 
+        onChange={(txt) => this.handleIpAdrressChange(txt) } 
+        onKeyPress={this.handlePressEnter}
+      />
+      <button 
+        type="button" 
+        onClick={ () => this.connectToWS(this.state.ip_address) }
+      > Connect </button>
+    </form>
+      
+    </header>
+  </div>)
+  }
+
+  render() {
+
+    var current = this.state.ws ? this.renderButtons() : this.renderConnectButton()
+    console.log(this.state.ws ? 'is connected!' : 'is not connected...')
+    return (
+      <div>
+        {current}
       </div>
     );
   }
